998127950f0950ebc3572b9b6ee87b5730fd2d2c
Jihyeok Seo <me@limeburst.net>
2012-05-13 16:40:54 +0900

    template-ized the default config file

--- yak/data/_config.py
+++ yak/data/_config.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
-TITLE = u"Just another Yak blog"
-SUBTITLE = u"Just another Yak blog"
-AUTHOR = u"Yak Shaver"
-EMAIL = u""                                 # Used in the ATOM feed. Not necessary.
-RIGHTS = u"Copyright © 2012 Yak Shaver."    # Used in the ATOM feed.
-URL = u"http://example.com/"    # Used in the ATOM feed. Be sure to set this correctly.
-                                # Trailing slash does not matter.
+TITLE = u"{{ blog.TITLE }}"         # Name of the blog.
+SUBTITLE = u"{{ blog.SUBTITLE }}"   # Blog description.
+AUTHOR = u"{{ blog.AUTHOR }}"       # The author of this blog.
+EMAIL = u"{{ blog.EMAIL }}"     # Used in the ATOM feed. Not necessary.
+RIGHTS = u"{{ blog.RIGHTS }}"   # Used in the ATOM feed.
+URL = u"{{ blog.URL }}"         # Used in the ATOM feed. Be sure to set this correctly. Trailing slash does not matter.
 
-# The baked blog will end up here.
-# Yak will delete everything under this directory before baking the blog, so be careful.
-OUTPUT_DIRECTORY = u"_site"
+# The baked blog will end up here.  Yak will delete everything under this directory before baking the blog, so be sure to set this correctly.
+OUTPUT_DIRECTORY = u"{{ blog.OUTPUT_DIRECTORY }}"
 
-# Yak server settings for this blog.
-SERVER_PORT = 5000
-SERVER_USERNAME = u"yak"
-SERVER_PASSWORD = u"yak"
+# Yak blog management server settings.
+SERVER_PORT = {{ blog.SERVER_PORT }}
+SERVER_USERNAME = u"{{ blog.SERVER_USERNAME }}"
+SERVER_PASSWORD = u"{{ blog.SERVER_PASSWORD }}"
