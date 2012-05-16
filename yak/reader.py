2e10e81ad53cae2e9446fedf662b774bb182eda1
Jihyeok Seo <me@limeburst.net>
2012-05-16 16:54:09 +0900

    refactored the reading code

--- yak/reader.py
+++ yak/reader.py
@@ -6,83 +6,38 @@ from codecs import open
 from datetime import datetime
 from markdown import Markdown
 
-md = Markdown(extensions=['extra', 'meta'])
-
 class Post(object):
-    def __init__(self, root, filename, slug, title, content, url, link, published, updated):
-        self.root = root
-        self.filename = filename
-        self.slug = slug
-        self.title = title
-        self.content = content
-        self.url = url
-        self.link = link
-        self.published = published
-        self.updated = updated
+    def __init__(self, filename, post):
+        self.root = filename['root']
+        self.filename = filename['filename']
+        self.slug = filename['slug']
+        self.url = filename['url']
+        self.title = post['title']
+        self.markdown = post['markdown']
+        self.html = post['html']
+        self.link = post['link']
+        self.published = post['published']
+        self.published_humanized = datetime.strftime(self.published, "%B %d, %Y")
+        self.published_rfc3999 = datetime.strftime(self.published, "%Y-%m-%dT%H:%M:%SZ")
+        self.updated = post['updated']
+        self.updated_rfc3999 = datetime.strftime(self.updated, "%Y-%m-%dT%H:%M:%SZ")
 
-def valid_post(root, filename):
-    """
-        Validates a post file and returns a Post object, or returns False
-    """
-    if filename.endswith('.md'):
-        # Only handle files with proper name format. ex) 2012-01-01-hello-world.md
-        try:
-            published = datetime.strptime(filename[:10], '%Y-%m-%d')
-        except ValueError:
-            return False
-        slug = filename[11:-3]
-        if not slug:
-            return False
-
-        # Now get some real data
-        with open(os.path.join(root, filename), 'r', 'utf-8') as f:
-            content = md.convert(f.read())
-            if not content: return False
-
-            # Get title
-            try:
-                title = md.Meta['title'][0]
-            except KeyError:
-                title = slug
-            except AttributeError:
-                return False
-
-            # Get published time
-            try:
-                time = datetime.strptime(md.Meta['time'][0], "%H:%M:%S")
-            except (ValueError, KeyError):
-                pass
-            else:
-                published = published.replace(
-                        hour=time.hour,
-                        minute=time.minute,
-                        second=time.second)
-
-            # Get updated datetime
-            try:
-                updated = md.Meta['updated'][0]
-            except KeyError:
-                updated = published
-            else:
-                try:
-                    updated = datetime.strptime(updated, '%Y-%m-%d %H:%M:%S')
-                except ValueError:
-                    try:
-                        updated = datetime.strptime(updated, '%H:%M:%S')
-                    except ValueError:
-                        updated = published
-
-            # Get post and linked URL
-            url = datetime.strftime(published, "%Y/%m/%d/{}/".format(slug))
-            try:
-                link = md.Meta['link'][0]
-            except KeyError:
-                link = url
-
-        return Post(root.decode('utf-8'), filename.decode('utf-8'), slug.decode('utf-8'),
-                title, content, url.decode('utf-8'), link.decode('utf-8'), published, updated)
+def read_settings(blog_dir):
+    tempdict = {}
+    settings = DEFAULT_CONFIG.copy()
+    try:
+        execfile(os.path.join(blog_dir, '_config.py'), tempdict)
+    except IOError:
+        pass
     else:
-        return False
+        for key in tempdict:
+            if key.isupper():
+                settings[key] = tempdict[key]
+    if not settings['URL'].endswith('/'):
+        settings['URL'] += '/'
+    settings['PATH'] = os.path.abspath(blog_dir)
+    settings['OUTPUT_DIRECTORY'] = os.path.join(settings['PATH'], settings['OUTPUT_DIRECTORY'])
+    return settings
 
 def get_posts(post_dir):
     """
@@ -91,7 +46,101 @@ def get_posts(post_dir):
     posts = []
     for root, _, files in os.walk(post_dir):
         for filename in files:
-            post = valid_post(root, filename)
-            if post:
-                posts.append(post)
+            valid_filename = is_valid_filename(root, filename)
+            if valid_filename:
+                with open(os.path.join(root, filename), 'r', 'utf-8') as f:
+                    markdown = f.read()
+                valid_post = is_valid_post(markdown, valid_filename['published'])
+                if valid_post:
+                    posts.append(Post(valid_filename, valid_post))
     return posts
+    
+def get_postlist(post_dir):
+    postlist = []
+    for root, _, files in os.walk(post_dir):
+        for filename in files:
+            valid_filename = is_valid_filename(root, filename)
+            if valid_filename:
+                postlist.append(valid_filename)
+    return postlist
+
+def is_valid_filename(root, filename):
+    # Only handle files with proper name format. ex) 2012-01-01-hello-world.md
+    if filename.endswith('.md'):
+        try:
+            published = datetime.strptime(filename[:10], '%Y-%m-%d')
+        except ValueError:
+            return False
+        slug = filename[11:-3]
+        if not slug:
+            return False
+        url = datetime.strftime(published, "%Y/%m/%d/{}/".format(slug))
+        return {
+                'root': root.decode('utf-8'),
+                'filename': filename.decode('utf-8'),
+                'slug': slug.decode('utf-8'),
+                'published': published,
+                'url': url.decode('utf-8'),
+                }
+    else:
+        return False
+
+
+def is_valid_post(markdown, published):
+    """
+        Validates post markdown content and returns a dict of post data, or False.
+    """
+
+    # Get HTML data from markdown
+    md = Markdown(extensions=['extra', 'meta'])
+    html = md.convert(markdown)
+    if not html:
+        return False
+
+    # Get title
+    try:
+        title = md.Meta['title'][0]
+    except (AttributeError, KeyError):
+        return False
+
+    # Get published time
+    try:
+        time = datetime.strptime(md.Meta['time'][0], "%H:%M:%S")
+    except (KeyError, ValueError):
+        pass
+    else:
+        published = published.replace(
+                hour=time.hour,
+                minute=time.minute,
+                second=time.second)
+
+    # Get updated datetime
+    try:
+        updated = md.Meta['updated'][0]
+    except KeyError:
+        updated = published
+    else:
+        try:
+            updated = datetime.strptime(updated, '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            try:
+                updated = datetime.strptime(updated, '%H:%M:%S')
+            except ValueError:
+                updated = published
+    if updated < published:
+        return False
+
+    # Get linked URL
+    try:
+        link = md.Meta['link'][0]
+    except KeyError:
+        link = None
+
+    return {
+            'markdown': markdown,
+            'html': html,
+            'title': title,
+            'published': published,
+            'updated': updated,
+            'link': link,
+            }
