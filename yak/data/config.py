aedd8121c42cbc51a9eaef37a66106ec79a493a8
Jihyeok Seo <me@limeburst.net>
2012-07-15 01:03:34 +0900

    deal with backslashes. best practice? i don't think so

--- yak/data/config.py
+++ yak/data/config.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-TITLE = u"{{ blog.TITLE }}" # Name of the blog.
-SUBTITLE = u"{{ blog.SUBTITLE }}" # Blog description.
-AUTHOR = u"{{ blog.AUTHOR }}" # The author of this blog.
-URL = u"{{ blog.URL }}" # Used in the ATOM feed. Be sure to set this correctly. Trailing slash does not matter.
+TITLE = ur"{{ blog.TITLE }}" # Name of the blog.
+SUBTITLE = ur"{{ blog.SUBTITLE }}" # Blog description.
+AUTHOR = ur"{{ blog.AUTHOR }}" # The author of this blog.
+URL = ur"{{ blog.URL }}" # Used in the ATOM feed. Be sure to set this correctly. Trailing slash does not matter.
 
 # The baked blog will end up here.  Yak will delete everything under this directory before baking the blog, so be sure to set this correctly.
-OUTPUT_DIRECTORY = u"{{ blog.OUTPUT_DIRECTORY }}"
+OUTPUT_DIRECTORY = ur"{{ blog.OUTPUT_DIRECTORY }}"
